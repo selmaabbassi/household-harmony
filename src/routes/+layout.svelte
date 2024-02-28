b5656f8f976978a4cf515cc042074e0a0a5bd933
Selma Abbassi <selma.abbassi@lm.se>
2024-02-28 19:56:29 +0100

    Fixed issue with footer

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,6 +5,5 @@
 </script>
 
 <Menu />
-<Footer />
-
 <slot />
+<Footer />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,15 @@
-<div class="hero min-h-screen" style="background-image: url(/hero.jpg)">
-  <div class="hero-overlay bg-opacity-60"></div>
-  <div class="hero-content text-center text-neutral-content">
-    <div class="max-w-md">
-      <h1 class="mb-5 text-4xl font-bold">Household Harmony</h1>
-      <p class="mb-5">
-        Manage your household by completing tasks and earning rewards to upgrade
-        your character!
-      </p>
-      <button class="btn btn-primary">Get Started</button>
+<div class="container h-screen mx-auto">
+  <div class="hero h-4/5" style="background-image: url(/hero.jpg)">
+    <div class="hero-overlay bg-opacity-50"></div>
+    <div class="hero-content text-center text-neutral-content">
+      <div class="max-w-md">
+        <h1 class="mb-5 text-4xl font-bold">Household Harmony</h1>
+        <p class="mb-5">
+          Manage your household by completing tasks and earning rewards to
+          upgrade your character!
+        </p>
+        <button class="btn btn-primary">Get Started</button>
+      </div>
     </div>
   </div>
 </div>
