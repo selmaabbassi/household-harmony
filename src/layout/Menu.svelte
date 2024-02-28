f5cf05266defde3d3d3d7bb927d948fe510c3b55
Selma Abbassi <selma.abbassi@lm.se>
2024-02-28 09:15:33 +0100

    Fixed routes, pages and layout conforming to svelte standard

---
+++ src/layout/Menu.svelte
@@ -0,0 +1,31 @@
+<script>
+  import Icon from "@iconify/svelte";
+</script>
+
+<div class="navbar">
+  <div class="navbar-start"></div>
+  <div class="navbar-center">
+    <a class="btn btn-ghost text-xl" href="/"
+      >Home <Icon icon="solar:home-bold" class="big-icon" /></a
+    >
+    <a class="btn btn-ghost text-xl" href="/tasks"
+      >Tasks <Icon icon="material-symbols:task" class="big-icon" /></a
+    >
+    <a class="btn btn-ghost text-xl" href="/rewards"
+      >Rewards <Icon icon="material-symbols:rewarded-ads" class="big-icon" /></a
+    >
+    <a class="btn btn-ghost text-xl" href="/household"
+      >Household <Icon
+        icon="material-symbols:family-restroom"
+        class="big-icon"
+      /></a
+    >
+  </div>
+  <div class="navbar-end"></div>
+</div>
+
+<style lang="postcss">
+  div :global(.big-icon) {
+    font-size: 35px;
+  }
+</style>
--- src/menu/Home.svelte
+++
@@ -1,7 +0,0 @@
-<script>
-  import CreateTask from "../components/CreateTask.svelte";
-</script>
-
-<div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-  <CreateTask />
-</div>
