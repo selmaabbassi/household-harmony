f5149b2fa14af38a14e2cd7d39260232ebdb84c3
Selma Abbassi <selmaabbassi@live.se>
2024-03-08 19:37:29 +0100

    Added secrets file and moved sql code to mysql.js

--- src/routes/tasks/+page.server.js
+++ src/routes/tasks/+page.server.js
@@ -1,57 +1,25 @@
-import { mysqlconnFn } from "$lib/server/mysql";
+import { createTask, deleteTask, getTasks } from "$lib/server/mysql";
 
 export async function load() {
-  let mysqlconn = await mysqlconnFn();
-  try {
-    let results = await mysqlconn
-      .query("SELECT id, title, difficulty FROM db.tasks;")
-      .then(function ([rows, fields]) {
-        // console.log(rows);
-        return rows;
-      });
-
-    return {
-      tasks: Array.from(results),
-    };
-  } catch (error) {
-    console.error("Got an error!!!");
-    console.log(error);
-    return error;
-  }
+  return getTasks();
 }
-
-
 
 export const actions = {
   create: async ({ request }) => {
-    let mysqlconn = await mysqlconnFn();
     const data = await request.formData();
   
-    const taskTitle = data.get("task-title");
-    const difficulty = data.get("task-difficulty");
-  
-    try {
-      let results = await mysqlconn
-      .query('INSERT INTO db.tasks (title, difficulty) VALUES (?, ?)', [taskTitle, difficulty]);
-  
-    } catch(error) {
-      console.log("Error");
-      console.log(error);
+    const task = {
+      title: data.get("task-title"),
+      difficulty: data.get("task-difficulty")
     }
+
+    createTask(task);
   },
 
   delete: async ({ request }) => {
-    console.log("delete")
-    let mysqlconn = await mysqlconnFn();
     const data = await request.formData();
     const taskId = data.get("id");
-    console.log(taskId);
 
-    try {
-      let results = await mysqlconn
-      .query('DELETE from tasks WHERE tasks.id = (?)', [taskId])
-    } catch(error){
-
-    }
+    deleteTask(taskId);
   }
 };
--- src/routes/tasks/manage/+page.server.js
+++ src/routes/tasks/manage/+page.server.js
@@ -1,21 +1,25 @@
-import { mysqlconnFn } from "$lib/server/mysql";
+import { createTask, deleteTask, getTasks } from "$lib/server/mysql";
 
 export async function load() {
-  let mysqlconn = await mysqlconnFn();
-  try {
-    let results = await mysqlconn
-      .query("SELECT id, title, difficulty FROM db.tasks;")
-      .then(function ([rows, fields]) {
-        // console.log(rows);
-        return rows;
-      });
-
-    return {
-      tasks: Array.from(results),
-    };
-  } catch (error) {
-    console.error("Got an error!!!");
-    console.log(error);
-    return error;
-  }
+  return getTasks();
 }
+
+export const actions = {
+  create: async ({ request }) => {
+    const data = await request.formData();
+  
+    const task = {
+      title: data.get("task-title"),
+      difficulty: data.get("task-difficulty")
+    }
+
+    createTask(task);
+  },
+
+  delete: async ({ request }) => {
+    const data = await request.formData();
+    const taskId = data.get("id");
+
+    deleteTask(taskId);
+  }
+};
