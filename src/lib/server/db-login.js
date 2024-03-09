9707a680c36dca8ee076a12c012f1917d2a7798c
Selma Abbassi <selmaabbassi@live.se>
2024-03-09 20:36:55 +0100

    Fixed issue with firebase

--- src/lib/server/db-login.js
+++ src/lib/server/db-login.js
@@ -1,5 +1,32 @@
 import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import {
+  getFirestore,
+  collection,
+  getDocs,
+  addDoc,
+  deleteDoc,
+  doc,
+} from "firebase/firestore/lite";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCMLSubnL5IaxjxKWjK-Ig4jgEZirjN2Iw",
+  authDomain: "household-harmony-da08c.firebaseapp.com",
+  projectId: "household-harmony-da08c",
+  storageBucket: "household-harmony-da08c.appspot.com",
+  messagingSenderId: "337338432690",
+  appId: "1:337338432690:web:3dc98dd543a6bc38431dab",
+  measurementId: "G-KW0KJMFLQE",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
 const auth = getAuth();
 
 export async function createUser(user) {
--- src/lib/server/firebase.js
+++ src/lib/server/firebase.js
@@ -1,20 +1,28 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { setDoc } from "firebase/firestore";
-import { getFirestore, collection, getDocs, addDoc, deleteDoc, doc, updateDoc } from 'firebase/firestore/lite';
+import {
+  getFirestore,
+  collection,
+  getDocs,
+  addDoc,
+  deleteDoc,
+  doc,
+  updateDoc,
+} from "firebase/firestore/lite";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyCMLSubnL5IaxjxKWjK-Ig4jgEZirjN2Iw",
-    authDomain: "household-harmony-da08c.firebaseapp.com",
-    projectId: "household-harmony-da08c",
-    storageBucket: "household-harmony-da08c.appspot.com",
-    messagingSenderId: "337338432690",
-    appId: "1:337338432690:web:3dc98dd543a6bc38431dab",
-    measurementId: "G-KW0KJMFLQE"
+  apiKey: "AIzaSyCMLSubnL5IaxjxKWjK-Ig4jgEZirjN2Iw",
+  authDomain: "household-harmony-da08c.firebaseapp.com",
+  projectId: "household-harmony-da08c",
+  storageBucket: "household-harmony-da08c.appspot.com",
+  messagingSenderId: "337338432690",
+  appId: "1:337338432690:web:3dc98dd543a6bc38431dab",
+  measurementId: "G-KW0KJMFLQE",
 };
 
 // Initialize Firebase
@@ -22,51 +30,49 @@ const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
 export async function getTasks() {
-    const taskCollection = collection(db, 'tasks');
-    const taskDocs = await getDocs(taskCollection);
-    const taskList = taskDocs.docs.map(doc => {
-        const document = {
-            id: doc.id,
-            data: doc.data()
-        };
-        return document;
-    });
-
-    return {
-        tasks: Array.from(taskList),
+  const taskCollection = collection(db, "tasks");
+  const taskDocs = await getDocs(taskCollection);
+  const taskList = taskDocs.docs.map((doc) => {
+    const document = {
+      id: doc.id,
+      data: doc.data(),
     };
+    return document;
+  });
+
+  return {
+    tasks: Array.from(taskList),
+  };
 }
 
 export async function createTask(task) {
-    try {
-        const docRef = await addDoc(collection(db, 'tasks'), {
-            title: task.title,
-            difficulty: task.difficulty
-        });
-    } catch (error) {
-        console.error("Error adding document: ", error);
-    }
+  try {
+    const docRef = await addDoc(collection(db, "tasks"), {
+      title: task.title,
+      difficulty: task.difficulty,
+    });
+  } catch (error) {
+    console.error("Error adding document: ", error);
+  }
 }
 
 export async function deleteTask(taskId) {
-    await deleteDoc(doc(db, 'tasks', taskId));
-
+  await deleteDoc(doc(db, "tasks", taskId));
 }
 
 export async function completeTask(taskId) {
-    const taskRef = doc(db, 'tasks', taskId);
+  const taskRef = doc(db, "tasks", taskId);
 
-    await updateDoc(taskRef, {
-        completed: true
-    });
+  await updateDoc(taskRef, {
+    completed: true,
+  });
 }
 
-
 export async function editTask(task) {
-    const taskRef = doc(db, 'tasks', task.id);
+  const taskRef = doc(db, "tasks", task.id);
 
-    await updateDoc(taskRef, {
-        title: task.title,
-        difficulty: task.difficulty
-    });
+  await updateDoc(taskRef, {
+    title: task.title,
+    difficulty: task.difficulty,
+  });
 }
